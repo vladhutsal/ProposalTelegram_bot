b2dfdfce2b6bd9f98ec08e2ab3c1c4a6edfdf38c
vlad <vl.hutsal@gmail.com>
2020-10-07 02:28:56 +0300

    add: ConversationHandler to divide user-bot dialor into stages add: change_text function to work with html template add: run_through_headers function to ask user fill the data for exact header of template

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+from bs4 import BeautifulSoup
 import telegram
 import logging
 from telegram.ext import (
@@ -7,36 +8,92 @@ from telegram.ext import (
     CommandHandler,
     MessageHandler,
     Filters,
+    ConversationHandler
 )
-headers = ['header1', 'header2', 'header3', 'header4']
 
-updater = Updater(token='1259603530:AAHRWl9xHFeoLncdt1jhXLC2ddFLh0YMHBg', use_context=True)
-dispatcher = updater.dispatcher
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+ 
+logger = logging.getLogger(__name__)
 
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                     level=logging.INFO)
+RUN_THROUGH_HEADERS, FILL_DATA = range(2)
 
+to_search = {
+    'name1': 'Name 1',
+    'name2': 'Name 2',
+    'name3': 'Name 3',
+    'name4': 'Name 4'
+}
 
 def start(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="hello")
+    context.user_data['headers'] = to_search
+    context.user_data['count'] = 0
 
-def get_text(update, context):
-    update = update.message.text
-    change_text(update)
+    context.bot.send_message(chat_id=update.effective_chat.id, text='I`ll help you to complete the proposal. Type something to begin.')
 
-def change_text(text):
+    return RUN_THROUGH_HEADERS
+
+
+def run_through_headers(update, context):
+    count = context.user_data['count']
+
+    current_header = f'name{count}'
+    context.user_data['headers'][current_header] = update.message.text
+    update.message.reply_text('Type next name')
+    context.user_data['count'] += 1
+    if context.user_data['count'] < 4:
+        return RUN_THROUGH_HEADERS
+    
+    return fill_data(context)
+    
+
+def fill_data(context):
+    print(context.user_data['answers_arr'])
+
+    return ConversationHandler.END
+
+
+def change_text(update, context):
     with open('static/index.html', 'r+') as page:
-        doc_content = page.read()
-        print(doc_content)
+        parser = BeautifulSoup(page, 'html.parser')
+    
+    for header in context.user_data['headers'].keys():
+        pass
+
+    tag_data = parser.find('p', tgname='name1')
+    tag_data.string.replace_with('bar')
+    res_content = parser.prettify()
+
+    with open('static/result.html', 'w+') as res_doc:
+        res_doc.write(res_content)
+
+
+def end():
+    pass
 
 
 def main():
-    start_handler = CommandHandler('start', start)
-    dispatcher.add_handler(start_handler)
+    updater = Updater(token='1259603530:AAHRWl9xHFeoLncdt1jhXLC2ddFLh0YMHBg', use_context=True)
+    dispatcher = updater.dispatcher
 
-    get_text_handler = MessageHandler(Filters.text & (~Filters.command), get_text)
-    dispatcher.add_handler(get_text_handler)
-    
+    conv_handler = ConversationHandler(
+        entry_points=
+        [
+            CommandHandler('start', start)
+        ],
+
+        states=
+        {
+            RUN_THROUGH_HEADERS: [MessageHandler(Filters.text, run_through_headers)],
+            FILL_DATA: [MessageHandler(Filters.text, fill_data)],
+        },
+
+        fallbacks=
+        [
+            CommandHandler('end', end)
+        ]
+    )
+
+    dispatcher.add_handler(conv_handler)  
     updater.start_polling()
 
 
