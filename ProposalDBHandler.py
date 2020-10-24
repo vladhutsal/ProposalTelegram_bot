42e2cb7e797b340b91980e8087320b2822bb4b4b
vlad <vl.hutsal@gmail.com>
2020-10-24 23:15:23 +0300

    add: serializer() and deserializer() to work with DB add: get_engineer() to get engineer by id update: engineers_in_proposal_id atribute moved from Proposal to ProposalDBHandler to make last one more independent from Proposal class instances add: get_all_engineers_in_proposal() - this method use engineers count, that`ve been added to current proposal add: 'table' atribute to ProposalDBHandler add: positional argument 'name' to ProposalDBHandler`s func 'create_table' - to be able to create different tables in db, like one for test_pdf.py. 'table' atribute is default one. update: create_connection() now work without db crash due to reusing connection open and close in different functions of class update: field names in db now match to field in engineers template

--- ProposalDBHandler.py
+++ ProposalDBHandler.py
@@ -1,12 +1,15 @@
 import sqlite3
 from sqlite3 import Error, IntegrityError
-from decorators import engineers_table_exists
+from Proposal import Proposal
 
 
-class ProposalDBHandler():
+class ProposalDBHandler(Proposal):
     def __init__(self, path):
+        Proposal.__init__(self)
         self.db_path = path
         self.table_exc = None
+        self.table = 'proposal'
+        self.engineers_in_proposal_id = []
 
     def create_connection(self):
         database_path = self.db_path
@@ -17,67 +20,90 @@ class ProposalDBHandler():
             print('Failed to connect to database', err)
         return conn
 
-    def create_engineers_table(self):
+    def create_table(self, name='engineers'):
         conn = self.create_connection()
-        table = """ CREATE TABLE IF NOT EXISTS engineers (
+        table = f""" CREATE TABLE IF NOT EXISTS {name} (
                     id integer PRIMARY KEY,
-                    name text NOT NULL UNIQUE,
-                    position text NOT NULL,
-                    email text NOT NULL,
-                    photo blob NOT NULL
+                    N text NOT NULL UNIQUE,
+                    P text NOT NULL,
+                    EM text NOT NULL,
+                    PHT text NOT NULL
                 ); """
         if conn is not None:
             try:
                 conn.execute(table)
                 return True
             except Error as err:
-                print('Unable to create Engineers table', err)
+                print(f'Unable to create {name} table', err)
         else:
             print('Not connected')
         conn.close()
         return False
 
-    @engineers_table_exists
-    def get_engineer_info(self, obj_id, field_name):
-        conn = self.create_connection()
-        curr = conn.cursor()
-        curr.execute(f"select {field_name} from engineers where id=?",
-                     (obj_id,))
-        field_content = curr.fetchall()
-        conn.close()
-        return self.serialize(field_content, 'obj')
+    def get_all_engineers_in_proposal(self):
+        engn_template = self.engineer_template
+        list_of_engn_dicts = []
+        proposal_ids = self.engineers_in_proposal_id
 
-    @engineers_table_exists
+        for engn_id in proposal_ids:
+            engn_content = self.get_engineer(engn_id)
+            for title_id in engn_template.keys():
+                engn_template[title_id][1] = engn_content.pop(0)
+            list_of_engn_dicts.append(engn_template)
+            self.reset_engineer_template()
+        return list_of_engn_dicts
+
+    def get_engineer(self, engn_id):
+        conn = self.create_connection()
+        cur = conn.cursor()
+        cur.execute(f'select * from {self.table} where id=?', (engn_id,))
+        engineer = self.deserialize(cur.fetchall(), 'fields', conn)[1:]
+        return engineer
+
     def get_all_engineers_id(self):
         conn = self.create_connection()
-        curr = conn.cursor()
-        curr.execute("select id from engineers")
-        engineers_id = curr.fetchall()
-        conn.close()
+        cur = conn.cursor()
+        cur.execute(f"select id from {self.table}")
+        engineers_id = cur.fetchall()
         if engineers_id:
-            return self.serialize(engineers_id, 'list')
+            return self.deserialize(engineers_id, 'column', conn)
         return None
 
-    @engineers_table_exists
-    def store_new_engineer_to_db(self, template_engineer_dict):
-        db_list = [field for field in template_engineer_dict.values()]
-        content = (db_list[0][1], db_list[1][1], db_list[2][1], db_list[3][1])
-
+    def get_field_info(self, obj_id, field_name):
         conn = self.create_connection()
-        curr = conn.cursor()
+        cur = conn.cursor()
+        cur.execute(f"select {field_name} from {self.table} where id=?",
+                    (obj_id,))
+        field_content = cur.fetchall()
+        return self.deserialize(field_content, 'field', conn)
+
+    def store_new_engineer_to_db(self, template_engineer_dict):
+        content = self.serialize(template_engineer_dict)
+        conn = self.create_connection()
+        cur = conn.cursor()
         try:
-            curr.execute(''' insert into engineers(name,position,email,photo)
+            cur.execute(f''' insert into {self.table}(N,P,EM,PHT)
                             values(?,?,?,?)''', content)
             conn.commit()
         except IntegrityError:
+            conn.close()
             return 'This engineer is already in db'
 
-        print(f'Engineer with id {curr.lastrowid} was added to DB')
+        print(f'Engineer with id {cur.lastrowid} was added to DB')
         conn.close()
 
-    def serialize(self, content, content_type):
-        data = [db_data[0] for db_data in content]
-        if content_type == 'list':
-            return data
-        elif content_type == 'obj':
-            return data[0]
+    def deserialize(self, content, content_type, conn):
+        if content_type == 'column':
+            # only works if ID from 0 to 9
+            conn.close()
+            return [field[0] for field in content]
+        elif content_type == 'fields':
+            conn.close()
+            return [field for field in content[0]]
+        elif content_type == 'field':
+            conn.close()
+            return content[0][0]
+    
+    def serialize(self, content):
+        db_list = [field for field in content.values()]
+        return (db_list[0][1], db_list[1][1], db_list[2][1], db_list[3][1])
