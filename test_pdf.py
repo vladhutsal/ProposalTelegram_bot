3157ed461ee68b5c1e074a538f8b28c3e1c91865
vlad <vl.hutsal@gmail.com>
2020-10-27 00:11:10 +0200

    updated to work correctly with Proposal.py instead of telegram_bot.py

--- test_pdf.py
+++ test_pdf.py
@@ -1,18 +1,22 @@
 from lorem_text import lorem
 from random import randint
+from templates import (
+    content_template,
+    info_template,
+    engineer_template
+)
 
 
 def create_lorem_dict(db_handler, proposal):
     db_handler.table = 'test'
     db_handler.create_table(name='test')
 
-    cntnt_tmplt = proposal.content_template
-    info_tmplt = proposal.info_template
     db_handler.engineers_in_proposal_id = [1, 2]
 
     puppet = {
         'N': ['Name', 'Puppet Vasya'],
         'P': ['Position', 'QA engineer'],
+        'RT': ['Rate', '85'],
         'EM': ['Email', 'vasya.puppet@u-tor.com'],
         'PHT': ['Photo', 'engineers_photo/111.jpg']
     }
@@ -20,6 +24,7 @@ def create_lorem_dict(db_handler, proposal):
     puppet2 = {
         'N': ['Name', 'Puppet Petya'],
         'P': ['Position', 'Team lead'],
+        'RT': ['Rate', '45'],
         'EM': ['Email', 'petya.puppet@u-tor.com'],
         'PHT': ['Photo', 'engineers_photo/222.jpg']
     }
@@ -27,17 +32,20 @@ def create_lorem_dict(db_handler, proposal):
     for engineer in puppet_engineers:
         db_handler.store_new_engineer_to_db(engineer)
 
-    for title_id in cntnt_tmplt.keys():
+    for title_id in content_template.keys():
         if '_list' in title_id:
             for x in range(randint(1, 4)):
-                cntnt_tmplt[title_id][1] += f'{lorem.sentence()}\n'
+                content_template[title_id][1] += f'{lorem.sentence()}\n'
         else:
-            cntnt_tmplt[title_id][1] = lorem.sentence()
+            content_template[title_id][1] = lorem.sentence()
+    print(content_template)
 
-    for title_id in info_tmplt.keys():
-        info_tmplt[title_id][1] = lorem.words(randint(2, 5))
+    for title_id in info_template.keys():
+        info_template[title_id][1] = lorem.words(randint(2, 5))
 
-    engineers = db_handler.get_all_engineers_in_proposal()
-
-    db_handler.table = 'proposal'
-    return cntnt_tmplt, info_tmplt, engineers
+    db_handler.table = 'engineers'
+    return {
+        'content_dict': proposal.get_colored_titles(content_template),
+        'info_dict': info_template,
+        'engineers_list': puppet_engineers
+    }
