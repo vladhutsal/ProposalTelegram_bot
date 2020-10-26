c729058143c48161a1b7f292d16de62640a8ac71
vlad <vl.hutsal@gmail.com>
2020-10-27 00:09:24 +0200

    add: work_with_connection() decorator, to create and close db connection add: conn and cur atributes to store current connection and cursor

--- ProposalDBHandler.py
+++ ProposalDBHandler.py
@@ -1,14 +1,16 @@
 import sqlite3
 from sqlite3 import Error, IntegrityError
-from Proposal import Proposal
+from templates import engineer_template
+from copy import deepcopy
 
 
 # check for table existance
-class ProposalDBHandler(Proposal):
-    def __init__(self, path):
-        Proposal.__init__(self)
-        self.db_path = path
+class ProposalDBHandler:
+    def __init__(self):
+        self.db_path = 'proposal.db'
         self.table = 'engineers'
+        self.conn = None
+        self.cur = None
 
         self.engineers_in_proposal_id = []
         self.engineers_rates = {}
@@ -22,8 +24,18 @@ class ProposalDBHandler(Proposal):
             print('Failed to connect to database', err)
         return conn
 
+    def work_with_connection(func):
+        def checker(self, *args, **kwargs):
+            self.conn = self.create_connection()
+            self.cur = self.conn.cursor()
+            if not self.conn.execute(f'select * from {self.table}'):
+                self.create_table(self)
+            res = func(self, *args, **kwargs)
+            self.conn.close()
+            return res
+        return checker
+
     def create_table(self, name='engineers'):
-        conn = self.create_connection()
         table = f""" CREATE TABLE IF NOT EXISTS {name} (
                     id integer PRIMARY KEY,
                     N text NOT NULL UNIQUE,
@@ -31,9 +43,9 @@ class ProposalDBHandler(Proposal):
                     EM text NOT NULL,
                     PHT text NOT NULL
                 ); """
-        if conn is not None:
+        if self.conn is not None:
             try:
-                conn.execute(table)
+                self.conn.execute(table)
                 return True
             except Error as err:
                 print(f'Unable to create {name} table', err)
@@ -41,80 +53,75 @@ class ProposalDBHandler(Proposal):
             print('Not connected')
         return False
 
-    def get_all_engineers_in_proposal(self):
+    def get_proposal_engineers(self):
+        # 1. Create list_of_engn_dicts - list of dicts based on engineer_template
+        # list_of_eng_dicts - all engineers in current proposal
+        # 2. Add rate to each dictionary in list_of_engn_dicts.
+        #
+        # This method used in html_to_pdf(), when all data for current proposal
+        # is retrieved from database and filled by user template-dicts.
+        # engineers_rates is a dict like: {'RT': ['name', 'rate']}
+        # field 'name' is used in show_title(), to show propper name of engineer,
+        # instead of ID, when asking user for Rate of current engineer.
         list_of_engn_dicts = []
         proposal_ids = self.engineers_in_proposal_id
-
+        # if something goes wrong, there will be two similar templates
         for engn_id in proposal_ids:
-            template = self.engineer_template
+            template = deepcopy(engineer_template)
             content_from_db = self.get_engineer(engn_id)
             for title_id in template.keys():
                 template[title_id][1] = content_from_db.pop(0)
-    # Add rate key to dictionary, that will be appended to list of engn dicts.
-    # This method used in html_to_pdf(), when all data for current proposal
-    # is retrieved from database and filled by user template-dicts.
-    # engineers_rates is a dict like: {'RT': ['name', 'rate']}
-    # field 'name' is used in show_title(), to show propper name of engineer,
-    # instead of ID.
+
             rate = self.engineers_rates[str(engn_id)][1]
             template['RT'] = ['Rate', rate]
             list_of_engn_dicts.append(template)
-            self.reset_engineer_template()
         return list_of_engn_dicts
 
+    @work_with_connection
     def get_engineer(self, engn_id):
-        conn = self.create_connection()
-        self.create_table()
-        cur = conn.cursor()
+        cur = self.cur
         cur.execute(f'select * from {self.table} where id=?', (engn_id,))
-        engineer = self.deserialize(cur.fetchall(), 'fields', conn)
+        engineer = self.deserialize(cur.fetchall(), 'fields')
         return engineer[1:]
 
-    def get_all_engineers_id(self):
-        conn = self.create_connection()
-        self.create_table()
-        cur = conn.cursor()
+    @work_with_connection
+    def get_engineers_id_list(self):
+        cur = self.cur
         cur.execute(f"select id from {self.table}")
         engineers_id = cur.fetchall()
         if engineers_id:
-            return self.deserialize(engineers_id, 'column', conn)
+            return self.deserialize(engineers_id, 'column')
         return None
 
+    @work_with_connection
     def get_field_info(self, obj_id, field_name):
-        conn = self.create_connection()
-        self.create_table()
-        cur = conn.cursor()
+        cur = self.cur
         cur.execute(f"select {field_name} from {self.table} where id=?",
                     (obj_id,))
         field_content = cur.fetchall()
-        return self.deserialize(field_content, 'field', conn)
+        return self.deserialize(field_content, 'field')
 
-    def store_new_engineer_to_db(self, template_engineer_dict):
-        content = self.serialize(template_engineer_dict)
-        conn = self.create_connection()
-        self.create_table()
-        cur = conn.cursor()
+    @work_with_connection
+    def store_new_engineer_to_db(self, engineer_dict):
+        content = self.serialize(engineer_dict)
+        conn = self.conn
+        cur = self.cur
         try:
             cur.execute(f''' insert into {self.table}(N,P,EM,PHT)
                             values(?,?,?,?)''', content)
             conn.commit()
         except IntegrityError:
-            conn.close()
             return 'This engineer is already in db'
 
         print(f'Engineer with id {cur.lastrowid} was added to DB')
-        conn.close()
 
-    def deserialize(self, content, content_type, conn):
+    def deserialize(self, content, content_type):
         if content_type == 'column':
             # only works if ID from 0 to 9
-            conn.close()
             return [field[0] for field in content]
         elif content_type == 'fields':
-            conn.close()
             return [field for field in content[0]]
         elif content_type == 'field':
-            conn.close()
             return content[0][0]
 
     def serialize(self, content):
